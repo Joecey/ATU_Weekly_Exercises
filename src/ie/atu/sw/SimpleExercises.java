377ab932dc4c9ce13cb2faccc1198be8b6a8f7aa
Joe Linogao <linogaoj@tcd.ie>
2024-01-27 15:16:30 +0000

    Finished week 1 exercises

---
+++ src/ie/atu/sw/SimpleExercises.java
@@ -0,0 +1,31 @@
+package ie.atu.sw;
+
+// alt + num keys to open menus
+// shift+f10 to compile and run
+// sout, ctrl+enter for suggestions and system out
+// ctrl+d duplicate
+// ctrl+y delete line
+// ctrl+shift+n open files
+
+public class SimpleExercises {
+    public static void main(String[] args) {
+        String firstname;
+        int TCD_year;
+        int ATU_year;
+
+        firstname = "Joe";
+        TCD_year = 2022;
+        ATU_year = 2024;
+
+        System.out.println("Hello world");
+        System.out.println("How are you?");
+        System.out.println("Hello person named:");
+        System.out.print(firstname);
+        System.out.print(" who graduated TCD in ");
+        System.out.print(TCD_year);
+        System.out.print(" and who started ATU in ");
+        System.out.print(ATU_year);
+
+
+    }
+}
