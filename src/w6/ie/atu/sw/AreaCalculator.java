a886cb2b7ffe1fde54096f6b9384981b97545c50
Joe Linogao <joe.linogao@akara.ai>
2024-03-03 13:48:55 +0000

    did week 6 things

--- src/w6/ie/atu/sw/AreaCalculator.java
+++ src/w6/ie/atu/sw/AreaCalculator.java
@@ -8,31 +8,53 @@ public class AreaCalculator {
 //    private boolean running = true; // use this to implement constant area calculator
 
     public AreaCalculator(){
-        scanner = new Scanner(System.in); // read from console now
+        scanner = new Scanner(System.in); // read from console now = specify scanner as reading from system in
         init();
         processInput();
     };
 
     public void calculateCircle(){
-        System.out.println("Calculating circle...");
+        System.out.println("Calculating circle... Enter Radius");
+        double radius = Double.parseDouble(scanner.next());
+
+        if (radius < 0) {
+            System.out.println("Cannot calculate area with negative number");
+        } else {
+            double area = Math.PI * Math.pow(radius, 2);
+            System.out.println("Area of circle is: " + area + " units squared");
+        }
+
     }
 
+
     public void calculateTriangle(){
-        System.out.println("Calculating triangle...");
+        System.out.println("Calculating triangle...Enter base");
+        double base = Double.parseDouble(scanner.next());
+
+        if (base < 0){
+            System.out.println("Cannot calculate area with negative number");
+            return;
+        }
+        System.out.println("Calculating triangle...Enter height");
+        double height = Double.parseDouble(scanner.next());
+
+        if (height < 0){
+            System.out.println("Cannot calculate area with negative number");
+        } else {
+            double area = 0.5 * base * height;
+            System.out.println("Area of triangle is: " + area + " units squared");
+        }
     }
 
     // handle scanner input
     private void processInput(){
         int option = Integer.parseInt(scanner.next());
-        if (option == 1){
-            calculateCircle();
-        }
 
-        else if (option == 2){
-            calculateTriangle();
-        }
-        else{
-            System.out.println("Exiting...");
+        switch (option) {
+            case 1 -> calculateCircle();
+            case 2 -> calculateTriangle();
+            case 3 -> System.out.println("Exiting...");
+            default -> System.out.println("Invalid selection bozo!!!");
         }
     }
     private void init(){
--- src/w6/ie/atu/sw/Runner.java
+++ src/w6/ie/atu/sw/Runner.java
@@ -4,7 +4,7 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Welcome to week 6");
         NLines.createNLines(2);
-        System.out.println("Examples of if statements");
+        System.out.println("Week 6 code example");
         AreaCalculator s = new AreaCalculator();
     }
 }
